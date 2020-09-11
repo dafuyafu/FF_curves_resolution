e20ce4d38297b87c0b247a4bd084fff420a8b907
dafuyafu <dafuyafu@MacBook-Pro.local>
2020-09-11 16:48:31 +0900

    remove resolution.py and rename test.py to resolution.py

--- resolution.py
+++ resolution.py
@@ -4,64 +4,69 @@
 	We will at first implemtent the outline of the Algorithm S-M* without any validation.
 
 '''
+from itertools import product
 
 from sympy.core.expr import Expr
+from sympy.core.function import diff
 from sympy.core.symbol import symbols
-from sympy.polys.polytools import poly, resultant, factor_list
-from sffpoly import SFF, SFFPoly, P2Point
+from sympy.polys.polytools import Poly, poly, resultant, factor_list
 
-def sing(f, **args):
+from sffpoly import sff, sffpoly, ff_solve, reduce
 
-	""" validates whether f in k[X,Y,Z]^h """
-
-	if not isinstance(f, Poly):
-		if not isinstance(f, Expr):
-			raise ValueError("Argument needs to be a Poly or Expr instance")
-		else:
-			f = poly(f)
-
-	var = symbols('x,y,z') # tuple
-	deg = f.degree()
-	sing = []
-	c = 0
-	for v in var:
-		_f = f.as_expr()
-		for u in var:
-			if v == u:
-				continue
-			_f = _f.subs({u: u * v})
-		_f = quo(_f, v ** deg)
-		_var = var.remove(v)
-		p = [resultant(_f, diff(_f, _var[0]), _var[1]), 
-			 resultant(_f, diff(_f, _var[1]), _var[0])]
-		sol = []
-		'''
-			Assume that only one of p[0] and p[1] has nonrational points.
-		'''
-		for i in range(len(_var)):
-			if _has_roots(p[i]):
-				sol.append(solve(p[i]))
-			else:
-				_rel_var = symbols('a_' + str(c))
-				c += 1
-				_rel = radical(p[i]).as_expr().subs({var[i]: _rel_var})
-				sol.append([_rel_var, - _rel_var])
-		points = direct_product(sol[0], sol[1])
-
-def _has_roots(f):
-	if not isinstance(f, Poly):
-		raise ValueError("needs to be Poly object")
-	if not len(f.gens) == 1:
-		raise ValueError("needs to be monovariate polynomial")
+def sing(f):
+	x, y, a, b = symbols('x y a b') # tuple
 	mod = f.get_modulus()
+	p = radical(resultant(f, diff(f, x), y), mod)
+	q = radical(resultant(f, diff(f, y), x), mod)
+	_list = []
+
+	if not _has_roots(p, x, mod):
+		_p = p.subs({x: a})
+		_list.append(_p)
+		_flag_p = True
+	else:
+		_p = 0
+		_flag_p = False
+	if not _has_roots(q, y, mod):
+		_q = q.subs({y: b})
+		_list.append(_q)
+		_flag_q = True
+	else:
+		_q = 0
+		_flag_q = False
+
+	_sff = sff(_list, mod)
+	_sff_p = sff(_p, mod)
+	_sff_q = sff(_q, mod)
+	f = sffpoly(f, _sff)
+	p = sffpoly(p, _sff_p)
+	q = sffpoly(q, _sff_q)
+
+	if _flag_p:
+		_sol_p = [{x: reduce(a ** (p.dom.mod ** e), _sff_p)} for e in range(p.degree())]
+	else:
+		_sol_p = [{x: n} for n in range(p.dom.mod) if p.subs({x: n}) == 0]
+	if _flag_q:
+		_sol_q = [{y: reduce(b ** (q.dom.mod ** e), _sff_q)} for e in range(q.degree())]
+	else:
+		_sol_q = [{y: n} for n in range(q.dom.mod) if q.subs({y: n}) == 0]
+	_sol = []
+
+	for point in product(_sol_p, _sol_q):
+		_point = {x: point[0][x], y: point[1][y]}
+		if p.subs(_point) == 0 and q.subs(_point) == 0 and f.subs(_point) == 0:
+			_sol.append(_point)
+	return _sol, _sff.as_SFF()
+
+def _has_roots(f, var, mod):
 	for i in range(mod):
-		if f.subs({f.gens[0]: i}) == 0:
+		if f.subs({var: i}) % mod == 0:
 			return True
 	return False
 
-def radical(f):
-	factors = factor_list(f.as_expr(), modulus=f.get_modulus())
+def radical(f, mod):
+	factors = factor_list(f.as_expr(), modulus=mod)
 	radical = 1
 	for fac in factors[1]:
 		radical *= fac[0]
-	return _wrap(radical, f)
+	return radical
